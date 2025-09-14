404a1f31e6c0007c9595ac2877086cfd69585fe6
Natalia Cuellar ALarcón <naty1503cuellar@gmail.com>
2025-09-14 17:57:48 -0500

    add: comentarios y variable

--- mi proyecto back-end/src/controllers/user.controller.js
+++ mi proyecto back-end/src/controllers/user.controller.js
@@ -1,5 +1,9 @@
 //Control de usuarios
 //user.controller.js
 // Modificar y proteger usuarios
-let User= "natts.c"
-let it="1061726558"
+const user={
+   username= "natts.c"
+ it="1061726558"
+ email="naty1503cuella@gmail.com"
+ password="134566"
+}
