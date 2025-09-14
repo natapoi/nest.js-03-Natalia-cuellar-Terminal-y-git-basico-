462bc2ebfd84aed725cdc7bb5c9b963033c822af
Natalia Cuellar ALarcón <naty1503cuellar@gmail.com>
2025-09-14 17:46:00 -0500

    add: agrego comentarios y ejemplo

--- mi proyecto back-end/README.md
+++ mi proyecto back-end/README.md
@@ -1,3 +1,2 @@
-Mi proyecto back-end
-Práctica de back-end con node.js
-Autor: Natalia Cuellar Alarcón-3106404024
+
+
---
+++ mi proyecto back-end/src/controllers/user.controller.js
@@ -0,0 +1,5 @@
+//Control de usuarios
+//user.controller.js
+// Modificar y proteger usuarios
+let User= "natts.c"
+let it="1061726558"
