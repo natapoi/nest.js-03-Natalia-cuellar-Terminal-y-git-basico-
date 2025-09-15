43c1e1f9d5dc5460063af2aba29c6bfe25b58b22
Natalia Cuellar ALarcón <naty1503cuellar@gmail.com>
2025-09-14 19:56:07 -0500

    Agregar conflicto desde prueba

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Conflicto
+Revisar conflicto
--- mi proyecto back-end/src/controllers/user.controller.js
+++ mi proyecto back-end/src/controllers/user.controller.js
@@ -1,9 +1,9 @@
 //Control de usuarios
 //user.controller.js
 // Modificar y proteger usuarios
-const user={
-   username= "natts.c"
- it="1061726558"
- email="naty1503cuella@gmail.com"
- password="134566"
-}
+const user= {
+   username: "natts.c",
+ itnum:"1061726558",
+ email:"naty1503cuella@gmail.com",
+ password:"134566"
+};
